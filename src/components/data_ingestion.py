69e2fd206eb877cc26a7e9dc176c1b71a441436c
Amandeepsinghn <amandeepsingh.kaillay@gmail.com>
2024-03-07 23:34:54 +0530

    data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,12 +7,14 @@ from textwrap import indent
 from operator import index
 
 from email import header
+from src.components import data_transformation
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -51,6 +53,3 @@ class DataIngestion:
             raise CustomException(e,sys)
             
 
-if __name__=="__main__":
-    obj=DataIngestion()
-    obj.initiate_data_ingestion()
