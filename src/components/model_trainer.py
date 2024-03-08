e891797b0c9b3d9f354ceb26f50f6733cbd7b36b
Amandeepsinghn <amandeepsingh.kaillay@gmail.com>
2024-03-09 00:28:38 +0530

    predict pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -24,7 +24,7 @@ from src.utlis import save_object, evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join('artifact','model.pkl')
+    trained_model_file_path=os.path.join('artifacts','model.pkl')
 
 class ModelTrainer:
     def __init__(self):
@@ -41,18 +41,59 @@ class ModelTrainer:
             )
 
             models={
-                "RANDOM FOREST": RandomForestRegressor(),
+                "Random Forest": RandomForestRegressor(),
                 "Decision tree": DecisionTreeRegressor(),
-                'Gradient bossitng': GradientBoostingRegressor(),
+                'Gradient Boositng': GradientBoostingRegressor(),
                 'Linear Regression' : LinearRegression(),
-                'K-Neighbors Classifier': KNeighborsRegressor(),
-                'XGBClassifier': XGBRegressor(),
-                'Catboosting classifier': CatBoostRegressor(verbose=False),
-                'AdaBoost Classifier' : AdaBoostRegressor(),
+                'XGBRegressor': XGBRegressor(),
+                'CatBoosting Regressor': CatBoostRegressor(verbose=False),
+                'AdaBoost Regressor' : AdaBoostRegressor(),
             }
 
+            params={
+                "Decision tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boositng":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+
+
+
+
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test
-                                             ,models=models)
+                                             ,models=models,params=params)
             
 
             best_model_score=max(sorted(model_report.values()))
--- src/utlis.py
+++ src/utlis.py
@@ -4,7 +4,9 @@ import numpy as np
 import pandas as pd 
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -21,13 +23,18 @@ def save_object(file_path,obj):
     
 
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     try:
         report={}
 
         for i in range(len(list(models))):
             model= list(models.values())[i]
+            para=params[list(models.keys())[i]]
 
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred= model.predict(X_train)
@@ -46,5 +53,13 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
         raise CustomException(e,sys)
     
 
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
     
 
